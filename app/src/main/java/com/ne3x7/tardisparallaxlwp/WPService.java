7a32bf22cfed9dfb08571e70c935fd79fbd75fde
ne3x7 <nickstulov@gmail.com>
2016-02-03 18:50:27 +0300

    Development of WPService.class

--- app/src/main/java/com/ne3x7/tardisparallaxlwp/WPService.java
+++ app/src/main/java/com/ne3x7/tardisparallaxlwp/WPService.java
@@ -1,4 +1,39 @@
 package com.ne3x7.tardisparallaxlwp;
 
-public class WPService {
+import android.service.wallpaper.WallpaperService;
+import android.view.SurfaceHolder;
+
+public class WPService extends WallpaperService{
+
+    @Override
+    public Engine onCreateEngine() {
+        return new WPEngine();
+    }
+
+    private class WPEngine extends Engine {
+
+        private int width;
+        private int height;
+
+        public WPEngine() {
+
+        }
+
+        @Override
+        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            super.onSurfaceChanged(holder, format, width, height);
+            this.width = width;
+            this.height = height;
+        }
+
+        @Override
+        public void onSurfaceDestroyed(SurfaceHolder holder) {
+            super.onSurfaceDestroyed(holder);
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+        }
+    }
 }
